105a069c4ca5104571cfb646c80d17636a2d2fc7
turatistefanogalvani <turati.stefano@iisgalvanimi.edu.it>
2026-04-17 11:29:09 +0000

    meta

---
+++ turati02.1/src/app/user-list/user-list.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './user-list.component.html',
+  styleUrl: './user-list.component.css'
+})
+export class UserListComponent{
+  names: string[]; //Creiamo il nostro vettore di stringhe 
+  constructor() { 
+     this.names = ['Ari', 'Carlos', 'Felipe', 'Nate']; //Riempiamo il vettore
+  }
+  ngOnInit() {
+  }
+}
